c85c9481856e59fffe0ee7d6cf82cbc0180306d5
Prince <princesuke@gmail.com>
2026-04-19 11:35:38 +0700

    feat: add counter store

--- src/components/level/Level3.jsx
+++ src/components/level/Level3.jsx
@@ -1,8 +1,15 @@
+import { useCounterStore } from "../../store/counterStore"
+
 export default function Level3({score, setScore}) {
+
+    const increment = useCounterStore((state) => state.increment);
+
     return <div className="p-3">
         <h3 className="font-bold">Level3</h3>
         <p>score: {score}</p>
         <br/>
         <button onClick={()=> setScore(score + 1)} className="border-2 p-2 cursor-pointer">เพิ่มคะแนน</button>
+        <br/><br/><br/>
+         <button onClick={increment} className="border-2 p-2 cursor-pointer">เพิ่ม counter</button>
     </div>
 }
--- src/pages/ParentLevel.jsx
+++ src/pages/ParentLevel.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
 import Level1 from "../components/level/Level1";
+import { useCounterStore } from "../store/counterStore";
 
 export default function ParentLevel() {
   const [score, setScore] = useState(0);
+
+  const count = useCounterStore((state) => state.count)
 
   return (
     <div>
       <h2>Level Page</h2>
       <p>score: {score}</p>
       <Level1 score={score} setScore={setScore}/>
+      <hr />
+      <div className="p-8 text-3xl">counter: {count}</div>
     </div>
   );
 }
